858e2df09def817af1c491dba9cd4995cdb0a3b2
Victor Hugo <victor-f250@hotmail.com>
2022-09-12 15:21:01 -0300

    novo

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 
 function App() {
 
-  
+  const name = "Super Veloz";
+  const newName = name.toUpperCase();
+
+  function soma(a, b){
+    return a+b;
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>REACT</h1>
+      <p>Você está acessando {name}</p>
+      <p>Nome maiúsculo {newName} </p>
+      <p>Valor soma:{soma(2, 3)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
