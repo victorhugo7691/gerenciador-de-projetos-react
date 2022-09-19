483e8848612b5d32b622a78188a7ca315dd188c8
Victor Hugo <victor-f250@hotmail.com>
2022-09-19 16:18:21 -0300

    front

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,17 +4,22 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Navbar from './components/layout/Navbar';
+import Container from './components/layout/Conteiner';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <Routes>
-        <Route exact path="/" element={<Home/>}></Route>
-        <Route path="/company" element={<Company/>}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/newproject" element={<NewProject/>}></Route>
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/newproject" element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+      <Footer/>
     </Router>
   );
 }
--- costs/src/components/layout/Navbar.js
+++ costs/src/components/layout/Navbar.js
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
+import logo from '../../img/cifrao.png'
+import Container from './Conteiner'
 
 function Navbar(){
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/company">Company</Link>
-            <Link to="/newproject">NewProject</Link>
-        </div>
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to="/">
+                    <img src={logo} alt="Costs" className={styles.tamanho_icone}/>
+                </Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}><Link to="/contact">Contact</Link></li>
+                    <li className={styles.item}><Link to="/company">Company</Link></li>
+                    <li className={styles.item}><Link to="/newproject">NewProject</Link></li>
+                </ul>  
+            </Container>
+        </nav>
     )
 }
 
